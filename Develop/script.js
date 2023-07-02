a04517321abc9a3ffa713f9aca19334f1486bea4
Britain Gilgour <britaing@gmail.com>
2023-07-02 14:45:52 -0500

    ajusted initial variables; noted possible arrays version JIC, added prompt messages to use

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,29 @@
 // Assignment code here
 
-var generateBtn = function(){
-  var
-}
+
+//variable for password length: 8 < 128
+var characterLength = "";
+//variable for character types(below); abcdefghijklmnopqrstuvwxyz(lower/upper), !?@#$%^&*(), 0123456789
+var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharactersTypes = "!?@#$%^&*()";
+var numbersChoice = "0123456789";
 
 
-//variable for password length: 8 < 128; and alert prompt
-let passwordLength = 25;
+//window prompt messages to use when figured out
+window.prompt("Choose a number between that is at least 8 and no greater than 128.");
+window.prompt("Include lowercase letter?");
+window.prompt("Include uppercase letters?");
+window.prompt("Include special characters?");
 
-if (passwordLength >= 8 && passwordLength <= 128) {
-  window.alert(hello);
-}
-
-
-
-//variable for character types(below); and alert prompt
-// abcdefghijklmnopqrstuvwxyz(lower/upper), !?@#$%^&*(), 0123456789(space?)
-var characterTypes = ;
-
-//math function? ceiling/random
+//math function? ceiling/random: Math.floor(Math.Random() * ??)
 
 
 //validate criteria: character types with ||, else not valid?
 
 
 
-function 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -42,10 +40,17 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//call the function?
+function generatePassword();
 
 
+// NOTES BELOW!!!!!!!!!!!!!!!
 
-
+// variables as arrays?
+// var lowerCaseLetters = ["a","b","c","d","e","f","g","h", "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// var specialCharactersTypes = ["!","?","@","#","$","%","^","&","*","(",")"];
+// var numbersChoice = ["0","1","2","3","4","5","6","7","8","9"];
 
 
 // GIVEN I need a new, secure password
