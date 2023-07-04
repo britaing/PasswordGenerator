89f8ae211d7cdc97a9edf160f8c4f88066fdeb7f
Britain Gilgour <britaing@gmail.com>
2023-07-04 13:17:54 -0500

    got help in tutoring on the code; added conditionals and got on right path

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,13 @@
 
 
 //variable for password length: 8 < 128
-var characterLength = "";
+// var characterLength = "";
 //variable for character types(below); abcdefghijklmnopqrstuvwxyz(lower/upper), !?@#$%^&*(), 0123456789
-var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialCharactersTypes = "!?@#$%^&*()";
-var numbersChoice = "0123456789";
+var lowerCaseLetters = ["a","b","c","d","e","f","g","h", "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharactersTypes = ["!","?","@","#","$","%","^","&","*","(",")"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
-
-//window prompt messages to use when figured out
-window.prompt("Choose a number between that is at least 8 and no greater than 128.");
-window.prompt("Include lowercase letter?");
-window.prompt("Include uppercase letters?");
-window.prompt("Include special characters?");
 
 //math function? ceiling/random: Math.floor(Math.Random() * ??)
 
@@ -28,6 +22,61 @@ window.prompt("Include special characters?");
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  //window prompt messages to use when figured out
+
+  
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+var characterLength =  window.prompt("Choose a number between that is at least 8 and no greater than 128.");
+console.log(characterLength);
+if (characterLength < 8 || characterLength > 128) {
+  alert('please select a number between 8 and 128.')
+  return null
+}
+
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+
+var lowerCaseChoice = window.confirm("Include lowercase letter?");
+var specialCharactersChoice = window.confirm("Include special characters?");
+var upperCaseChoice = window.confirm("Include uppercase letters?");
+var numbersChoice = window.confirm("Include numbers?");
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+if(lowerCaseChoice === false && specialCharactersChoice === false && upperCaseChoice === false && numbersChoice === false){
+  alert('please select one option')
+  return null
+}
+
+var userChoices = []
+
+if(lowerCaseChoice ===true){
+ userChoices = userChoices.concat(lowerCaseLetters)
+}
+if (specialCharactersChoice === true){
+  userChoices = userChoices.concat(specialCharactersTypes);
+  console.log(userChoices)
+}
+if(numbersChoice === true) {
+  userChoices = userChoices.concat(numbers)
+}
+
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+//  console.log(b);
+return 'Hi Britain'
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -40,8 +89,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//call the function?
-function generatePassword();
+
 
 
 // NOTES BELOW!!!!!!!!!!!!!!!
@@ -57,21 +105,3 @@ function generatePassword();
 
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
